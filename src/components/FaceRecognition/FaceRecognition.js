111db82bd27e0c0434cc512e45621670bc8a29ca
zvi741 <51421153+zvi741@users.noreply.github.com>
2020-04-26 09:31:02 +0300

    adding signin and register pages

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import './FaceRecognition.css'
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
-        <img src={imageUrl} alt="" width='500px' height='auto' />
+        <img
+          id='inputimage'
+          src={imageUrl}
+          alt=""
+          width='500px'
+          height='auto' />
+        <div
+          className='bounding-box'
+          style={{
+            left: box.leftCol,
+            right: box.rightCol,
+            top: box.topRow,
+            bottom: box.bottomRow
+          }}
+        ></div>
 
       </div>
     </div>
